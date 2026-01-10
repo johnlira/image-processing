c726d386d1f30617c532e8cd454f276c5f8fd5ce
John Lira <johnalsoares@gmail.com>
2026-01-09 21:48:46 -0300

    Chore: Update Docker compose, init.sql e env example

--- init.sql
+++ init.sql
@@ -4,7 +4,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Create users table
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)  NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
